1a54793394d568f9b83938cb010fe2e72e118378
Carlos Nazario <carlosnazario2010cn@gmail.com>
2024-10-02 18:07:03 -0300

    Removendo as dependencias do spring boot admin e do actuator

--- src/main/java/com/carlosnazario/carteira_de_acoes/CarteiraDeAcoesApplication.java
+++ src/main/java/com/carlosnazario/carteira_de_acoes/CarteiraDeAcoesApplication.java
@@ -1,11 +1,9 @@
 package com.carlosnazario.carteira_de_acoes;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
 public class CarteiraDeAcoesApplication {
 
     public static void main(String[] args) {
